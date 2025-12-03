45bef9da7007887032075c5f8bc7d617a658b251
Nicholas Sobchak <nsob79@gmail.com>
2025-12-03 17:27:08 +0000

    more class setup

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -6,3 +6,23 @@ Dictionary::~Dictionary()
 {
     delete m_root;
 }
+
+void Dictionary::loadFromFile(const string &filename)
+{
+}
+
+void insert(const string &word)
+{
+}
+
+bool search(const string &word)
+{
+}
+
+TrieNode findNode(const char &letter)
+{
+}
+
+void printDictionary()
+{
+}
