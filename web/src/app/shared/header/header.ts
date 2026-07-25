0be239f8a4e32df355693ac486eacd639547fa35
NicholasSobchak <nlsobchak@gmail.com>
2026-07-25 17:49:34 -0400

    add website navigation drawer

--- web/src/app/shared/header/header.ts
+++ web/src/app/shared/header/header.ts
@@ -7,4 +7,6 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  drawerOpen = false;
+}
